7f13dd23835357b472af8d5f649ebe47e74fe4aa
Svetlana Kuchuganova <56495509+s-kuchuganova@users.noreply.github.com>
2020-08-15 12:39:49 +0700

    add title and descriotion, enum class

--- test/start.py
+++ test/start.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI, UploadFile, File
+from enum import Enum
 import uvicorn
 import json
 import HW_model
 import LSTM_model
 
-app=FastAPI()
+class ModelName(str, Enum):
+    LSTM = "LSTM"
+    HW = "HW"
+
+app=FastAPI(title='Timeseries forecasting', description='Timeseries forecasting using LSTM and Holt-Winters model')
 
 @app.get("/")
 async def root():
     return {"message":"hello world" }
 
-@app.post("/predict/{index}")
-async def create_file(index: str, file: UploadFile=File(...)):
+@app.post("/predict/{model_name}")
+async def create_file(model_name: ModelName, file: UploadFile=File(...)):
     content= await file.read()
     json_obj = json.loads(content.decode('utf8'))
-    if index=="LSTM":
+    if model_name=ModelName.LSTM:
         model = Net(LSTM_model.input_dim, LSTM_model.hidden_dim, LSTM_model.output_dim)
         model.to(LSTM_model.device)
         model=LSTM_model.load_model('LSTM_model.pth')
@@ -24,12 +29,10 @@ async def create_file(index: str, file: UploadFile=File(...)):
         pred = LSTM_model.predict(model, train, 119) #пока не знаю что за загадочное число 119
         return {"predict": pred}
 
-    elif index=="HW":
+    if model_name=ModelName.HW:
         pred=HW_model.hw_classifier(json_obj)
         return {"predict": pred}
 
-    else:
-        return {"result": 0}
 
 
 if __name__ == "__main__":
